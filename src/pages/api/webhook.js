782df3b3d23a77e7e1052ef2e9490343826703be
61130061 <thezeroyou@gmail.com>
2023-03-22 13:00:24 +0700

    ➕ add @line/bot-sdk to make life easier

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,92 +1,121 @@
 import https from 'https';
+import { Client, middleware, JSONParseError } from '@line/bot-sdk';
 
 const TOKEN = process.env.LINE_ACCESS_TOKEN
+const LIFF_URL = process.env.LINE_ACCESS_TOKEN 
 
-export default function handler(req, res) {
+const lineConfig = {
+  channelAccessToken: TOKEN,
+  channelSecret: process.env.LINE_SECRET
+}
 
-  function reply(replyToken, messages) {
-    const payload = JSON.stringify({
-      replyToken,
-      messages
-    });
-    // Request header
-    const headers = {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer " + TOKEN
+const standupMenu = {
+  "type": "text",
+  "text": "I'm here! What would you like me todo?",
+  "quickReply": {
+    "items": [
+      {
+        "type": "action",
+        "action": {
+          "type": "uri",
+          "label": "Create workspace",
+          "uri": LIFF_URL + "/workspace/create"
+        }
+      },
+      {
+        "type": "action",
+        "action": {
+          "type": "uri",
+          "label": "Edit workspace",
+          "uri": LIFF_URL + "/workspace/setting"
+        }
+      }
+    ]
+  }
+}
+
+const client = new Client(config);
+
+export default async function handler(req, res) {
+  const middlewareFunc = middleware(lineConfig);
+
+  try {
+    await middlewareFunc(req, res, () => {});
+  } catch (err) {
+    if (err instanceof JSONParseError) {
+      console.error('Error: Invalid JSON', err);
+      return res.status(400).send('Bad Request');
     }
-    // Options to pass into the request
-    const webhookOptions = {
-      "hostname": "api.line.me",
-      "path": "/v2/bot/message/reply",
-      "method": "POST",
-      "headers": headers,
-      "body": payload
-    }
-    // Define request
-    const request = https.request(webhookOptions, (https_res) => {
-      https_res.on("data", (d) => {
-        // get data
-      })
-    })
-    // Handle error
-    request.on("error", (err) => {
-      console.error(err)
-    })
-    // Send data
-    request.write(payload)
-    request.end()
+    console.error(err);
+    return res.status(500).send('Internal Server Error');
   }
 
+  const questions = ['What did you do since yesterday?', 'What will you do today?']
+  const responses = [];
+
   if (req.method === 'POST') {
-    if (req.body.events.length === 0) {
-      console.log('recieve line webhook verify');
-      res.send("Send something to webhook URL!");
-    } else {
-      const event = req.body.events[0];
+    try {
+      const events = req.body.events;
 
-      if (event.type === "message") {
-        console.log('recieve message from user');
+      for (const event of events) {
+        if (event.type === 'message' && event.message.type === 'text') {
+          switch (event.message.text) {
+            case 'standup':
+              await client.replyMessage(event.replyToken, standupMenu);
+            default:
+              for (let i = 0; i < questions.length - 1; i++) {
+                // Send the a question to the user
+                await client.replyMessage(event.replyToken, {
+                  type: 'text',
+                  text: questions[i],
+                });
 
-        if (event.message.text === "standup") {
-          reply(event.replyToken, [
-            {
-              "type": "text",
-              "text": "I'm here! What would you like me todo?",
-              "quickReply": {
-                "items": [
-                  {
-                    "type": "action",
-                    "action": {
-                      "type": "uri",
-                      "label": "Create workspace",
-                      "uri": "https://liff.line.me/1660767408-j9VPQVOv/workspace/create"
-                    }
-                  },
-                  {
-                    "type": "action",
-                    "action": {
-                      "type": "uri",
-                      "label": "Create workspace",
-                      "uri": "https://liff.line.me/1660767408-j9VPQVOv/workspace/setting"
-                    }
-                  }
-                ]
+                // Wait for the user's response
+                const response = await waitForUserResponse(event.source.userId);
+
+                if (index < questions.length) {
+                  // Collect response and
+                  responses.push(response);
+                } else {
+                  // Thank for response
+                  await client.replyMessage(event.replyToken, [
+                    {
+                      type: 'text',
+                      text: 'Thank you for your answer!!',
+                    },
+                    {
+                      type: 'text',
+                      text: JSON.stringify(responses),
+                    },
+                  ]);
+                }
               }
-            }
-          ]);
-        } else {
-          reply(event.replyToken, [
-            {
-              "type": "text",
-              "text": "Hello sir, nice to meet you."
-            }
-          ]);
+          }
         }
       }
 
-      res.status(200).send('Success!');
+      res.status(200).json({ success: true });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ success: false });
     }
   } else {
-    res.status(200).send('hello from api');
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
   }
+}
+
+async function waitForUserResponse(userId) {
+  return new Promise((resolve, reject) => {
+    setInterval(async () => {
+      const messages = await client.getMessageContent(userId);
+      if (messages.length > 0) {
+        const message = messages[messages.length - 1];
+        if (message.type === 'text') {
+          clearInterval(interval);
+          resolve(message.text);
+        }
+      }
+    }, 1000);
+  });
 }
